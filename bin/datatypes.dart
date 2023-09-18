4cdc8742a56b5d88236ca7a7fde1d787f30bb47c
nazilkaradan <nazilkaradan3@gmail.com>
2023-09-18 13:03:23 +0530

    commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,14 @@
+void main() {
+  //variables
+  String name = 'nazil';
+  int age = 23;
+  double cgpa = 8.3;
+  String email = 'naz123@gmail.com';
+  //String Interpolation=combine variables to a predefined String using $,{}
+  //for single variable use $variablename
+  //more than one variable use ${variables}
+  print('my name is $name');
+  print('i am $age years old');
+  print('my cgpa is $cgpa');
+  print('my email is $email');
+}
---
+++ bin/first_sample.dart
@@ -0,0 +1,6 @@
+void main() {
+  print("nazil");
+  print(23);
+  print('no is 9874563210');
+  print(7.5);
+}
